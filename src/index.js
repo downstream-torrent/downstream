e8d5c87c426518ab1b80b74816fac6e94ad172b5
Brendan Goodenough <brendan@goodenough.nz>
2017-07-23 19:54:48 +1200

    Add ability to add torrents and emit progress

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
 import config from 'config'
 import http from 'http'
 import socket from 'socket.io'
+import WebTorrent from 'webtorrent'
 
-const app = http.createServer(handler)
+const app = http.createServer()
 const io = socket(app)
+const client = new WebTorrent()
 
 app.listen(config.get('port') || 3000)
 
-function handler (req, res) {
-  res.end('hello world')
+async function addTorrent (socket, uri) {
+  const torrent = await client.add(uri)
+  torrent.on('metadata', () => io.sockets.emit('torrentMetadata', {
+    infoHash: torrent.infoHash,
+    magnetUri: torrent.magnetURI,
+    path: torrent.path
+  }))
+  torrent.on('download', () => io.sockets.emit('torrentDownload', {
+    downloadSpeed: torrent.downloadSpeed,
+    infoHash: torrent.infoHash,
+    magnetUri: torrent.magnetURI,
+    path: torrent.path,
+    progress: torrent.progress,
+    uploadSpeed: torrent.uploadSpeed
+  }))
+  torrent.on('done', () => {
+    io.sockets.emit('torrentDone', torrent.magnetURI)
+  })
 }
 
 io.on('connection', (socket) => {
   console.log('Connection established!')
+  socket.on('add', (uri) => addTorrent(socket, uri))
 })
