436e2a6ba93da0cf3961a49d1e15915dc00525f3
Brendan Goodenough <brendan@goodenough.nz>
2017-07-28 22:13:19 +1200

    Refactor cli and torrent adding

--- src/server.js
+++ src/server.js
@@ -18,33 +18,17 @@ export const db = low('db.json', { storage: fileAsync })
 db.defaults({ torrents: [] }).write()
 db.get('torrents').map('infoHash').value().forEach(infoHash => addTorrent(infoHash))
 
-client.on('torrent', async torrent => {
-  const torrentInfo = {
-    infoHash: torrent.infoHash,
-    length: torrent.length,
-    magnet: torrent.magnetURI,
-    name: torrent.name,
-    path: torrent.path
-  }
-  if (!db.get('torrents').filter({ infoHash: torrent.infoHash }).size().value()) {
-    await db.get('torrents').push(torrentInfo).write()
-  } else {
-    await db.get('torrents').find({ infoHash: torrent.infoHash }).assign(torrentInfo).write()
-  }
-  io.sockets.emit('torrentAdded', torrentInfo)
+io.on('connection', async socket => {
+  console.log('Connection established!')
+  socket.on('add_torrent', uri => addTorrent(uri, socket))
+  socket.on('remove_torrent', torrentId => removeTorrent(torrentId))
+  socket.on('pause', torrentId => pauseTorrent(torrentId))
+  socket.on('resume', torrentId => resumeTorrent(torrentId))
+  socket.on('list_torrents', () => listTorrents(socket))
 })
 
 client.on('error', err => {
   console.log('Client Error:', err.message)
-})
-
-io.on('connection', async socket => {
-  console.log('Connection established!')
-  socket.on('add', uri => addTorrent(uri))
-  socket.on('remove', torrentId => removeTorrent(torrentId))
-  socket.on('pause', torrentId => pauseTorrent(torrentId))
-  socket.on('resume', torrentId => resumeTorrent(torrentId))
-  socket.on('list', () => listTorrents(socket))
 })
 
 // Scan feeds for new torrents
--- src/torrent.js
+++ src/torrent.js
@@ -3,52 +3,92 @@ import mv from 'mv'
 import path from 'path'
 import { client, db, io } from './server'
 
-export function addTorrent (uri) {
-  if (client.get(uri)) {
-    console.log(`Torrent ${uri} has already been added`)
+/**
+ * Updates the database and notify all clients whenever data is downloaded for
+ * a torrent.
+ *
+ * @param {object} torrent - the torrent with newly downloaded data
+ */
+export async function onDownload (torrent) {
+  const torrentInfo = {
+    downloaded: torrent.downloaded,
+    downloadSpeed: torrent.downloadSpeed,
+    uploaded: torrent.uploaded,
+    uploadSpeed: torrent.uploadSpeed,
+    progress: torrent.progress,
+    ratio: torrent.ratio,
+    numPeers: torrent.numPeers,
+    timeRemaining: torrent.timeRemaining
+  }
+  await db.get('torrents').find({ infoHash: torrent.infoHash }).assign(torrentInfo).write()
+  io.sockets.emit('torrent_download', torrentInfo)
+}
+
+/**
+ * Moves the completed torrent if the "complete" path is specified and differs
+ * from the download path. Updates the status of the torrent in the database
+ * and notifies all clients.
+ *
+ * @param {object} torrent - the torrent that has just completed
+ */
+export async function onDone (torrent) {
+  const downloadPath = config.get('paths.downloading')
+  const completePath = config.get('paths.complete')
+  if (completePath && completePath !== downloadPath) {
+    torrent.files.forEach(file => {
+      const oldPath = path.join(downloadPath, file.path)
+      const newPath = path.join(completePath, file.path)
+      mv(oldPath, newPath, { mkdirp: true }, err => {
+        if (err) {
+          io.sockets.emit('torrent_move_error', err.message)
+        }
+      })
+    })
+  }
+
+  const torrentEntry = db.get('torrents').find({ infoHash: torrent.infoHash }).assign({
+    path: completePath,
+    status: 'complete'
+  })
+  await torrentEntry.write()
+  io.sockets.emit('torrent_complete', torrentEntry.value())
+}
+
+export async function onTorrent (torrent, socket = null) {
+  const torrentInfo = {
+    infoHash: torrent.infoHash,
+    length: torrent.length,
+    magnet: torrent.magnetURI,
+    name: torrent.name,
+    path: torrent.path
+  }
+
+  if (!db.get('torrents').filter({ infoHash: torrent.infoHash }).size().value()) {
+    await db.get('torrents').push(torrentInfo).write()
+  } else {
+    await db.get('torrents').find({ infoHash: torrent.infoHash }).assign(torrentInfo).write()
+  }
+
+  if (socket) {
+    socket.broadcast.emit('torrent_added', torrentInfo)
+    socket.emit('torrent_added_reply', torrentInfo)
+  } else {
+    io.sockets.emit('torrent_added', torrentInfo)
+  }
+
+  torrent.on('download', () => onDownload(torrent))
+  torrent.on('done', () => onDone(torrent))
+}
+
+export function addTorrent (id, socket = null) {
+  if (client.get(id)) {
+    if (socket) {
+      socket.emit('torrent_add_error', 'Error: Torrent has already been added')
+    }
     return
   }
 
-  client.add(uri, { path: config.get('paths.downloading') }, torrent => {
-    console.log(`Added torrent ${torrent.infoHash}`)
-
-    // Send the progress of the torrent to all clients when data is downloaded.
-    torrent.on('download', async () => {
-      const torrentInfo = {
-        downloaded: torrent.downloaded,
-        downloadSpeed: torrent.downloadSpeed,
-        uploaded: torrent.uploaded,
-        uploadSpeed: torrent.uploadSpeed,
-        progress: torrent.progress,
-        ratio: torrent.ratio,
-        numPeers: torrent.numPeers,
-        timeRemaining: torrent.timeRemaining
-      }
-      await db.get('torrents').find({ infoHash: torrent.infoHash }).assign(torrentInfo).write()
-      io.sockets.emit('torrentDownload', torrentInfo)
-    })
-
-    // Move files to the completed directory once a torrent is complete. If the downloading
-    // and complete directories are the same the files will not be moved.
-    torrent.on('done', () => {
-      const downloadPath = config.get('paths.downloading')
-      const completePath = config.get('paths.complete')
-      if (completePath !== downloadPath) {
-        torrent.files.forEach(file => {
-          const oldPath = path.join(downloadPath, file.path)
-          const newPath = path.join(completePath, file.path)
-          mv(oldPath, newPath, { mkdirp: true }, err => {
-            if (err) {
-              io.sockets.emit('torrentMoveError', err.message)
-            }
-          })
-        })
-      }
-
-      // Send the magnet uri of the completed torrent to all clients.
-      io.sockets.emit('torrentDone', torrent.magnetURI)
-    })
-  })
+  client.add(id, { path: config.get('paths.downloading') }, torrent => onTorrent(torrent, socket))
 }
 
 export function removeTorrent (torrentId) {
@@ -59,7 +99,7 @@ export function removeTorrent (torrentId) {
     }
 
     console.log(`Removed torrent ${torrentId}`)
-    io.sockets.emit('remove', torrentId)
+    io.sockets.emit('torrent_removed', torrentId)
   })
 }
 
